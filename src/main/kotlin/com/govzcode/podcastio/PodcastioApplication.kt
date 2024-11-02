0d93c101ae8958cbbc82c4ebd73861fc83698067
rozoomcool <rozoom.um@gmail.com>
2024-11-02 16:41:20 +0300

    add auditing annotation

--- src/main/kotlin/com/govzcode/podcastio/PodcastioApplication.kt
+++ src/main/kotlin/com/govzcode/podcastio/PodcastioApplication.kt
@@ -2,10 +2,12 @@ package com.govzcode.podcastio
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableJpaAuditing
 class PodcastioApplication
 
 fun main(args: Array<String>) {
